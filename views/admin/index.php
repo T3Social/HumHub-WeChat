3ec67cc0999e34baed91c2c7c265c70f873a6015
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-03-20 19:55:36 -0400

    Update index.php

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,11 +1,14 @@
 <?php
-use humhub\compat\CActiveForm;
-use humhub\compat\CHtml;
+
+use yii
+use yii\bootstrap\ActiveForm;
+use yii\helpers\Html;
 use humhub\models\Setting;
 use humhub\modules\wechat\controllers\AdminController;
+
 ?>
 <div class="panel panel-default">
-	<div class="panel-heading"><?=Yii::t('WechatModule.base', '<strong>WeChat</strong>'); ?></div>
+	<div class="panel-heading"><?= Yii::t('WechatModule.base', '<strong>WeChat</strong>'); ?></div>
 	<div class="panel-body">
 		<script src="https://togetherjs.com/togetherjs-min.js"></script>
 		<button onclick="TogetherJS(this); return false;">Start TogetherJS</button>
